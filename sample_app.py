f01c8d57a5f860bf22aa84499d261ccc07e17618
Carlos Smith <mcmco421@gmail.com>
2020-11-17 15:15:27 +0700

    add Menu Bar and Tool Bar
    add Window icon

--- sample_app.py
+++ sample_app.py
@@ -1,6 +1,9 @@
-import sys 
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+import sys
+
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
 
 # https://realpython.com/python-menus-toolbars/
 
@@ -10,11 +13,25 @@ class Window(QMainWindow):
     def __init__(self, parent=None):
         """Initializer"""
         super().__init__(parent)
-        self.setWindowTitle("Python Menus & Toolbars")
+        self.setWindowTitle('Sample GUI')
+        self.setWindowIcon(QtGui.QIcon('vipkid.png'))
         self.resize(400, 200)
-        self.centralWidget = QLabel("Hello, World")
+        self.centralWidget = QLabel('Hello, World')
         self.centralWidget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
         self.setCentralWidget(self.centralWidget)
+        self._createMenuBar()
+        self._createToolBars()
+
+    def _createMenuBar(self):
+        menu_bar = self.menuBar()
+        file_menu = menu_bar.addMenu('&File')
+        edit_menu = menu_bar.addMenu('&Edit')
+        help_menu = menu_bar.addMenu('&Help')
+
+    def _createToolBars(self):
+        file_toolbar = self.addToolBar('File')
+        edit_toolbar = self.addToolBar('Edit')
+        help_toolbar = self.addToolBar('Help')
 
 
 if __name__ == "__main__":
