b756c7645859c1a59c3b21cd69f1448354a901e8
Carlos Smith <mcmco421@gmail.com>
2020-11-28 01:34:34 +0700

    Add 3 input variables from GUI for script: student, feedback, new student

--- Feedback_app.py
+++ Feedback_app.py
@@ -7,32 +7,48 @@ from PyQt5.QtWidgets import *
 
 
 class Window(QWidget):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         # Window options
         self.setWindowIcon(QtGui.QIcon('vipkid.png'))
         self.setWindowTitle('VIPKid Feedback App')
         self.resize(425, 500)
         # Create layout instance
         layout = QVBoxLayout()
+        # Widgets
+        self.student = QLineEdit(self)
+        self.yes_button = QRadioButton('&Yes')
+        self.no_button = QRadioButton('&No')
+        self.feedback_temp = QTextEdit(self)
+        self.feedback_output = QTextEdit(self)
+        self.copy_output = QPushButton('Copy Output Feedback')
         # Add widgets to layout
         layout.addWidget(QLabel('Student Name:'))
-        layout.addWidget(QLineEdit())
+        layout.addWidget(self.student)
         layout.addSpacing(4)
         layout.addWidget(QLabel('New Student?'))
-        layout.addWidget(QRadioButton('Yes'))
-        layout.addWidget(QRadioButton('No'))
+        layout.addWidget(self.yes_button)
+        layout.addWidget(self.no_button)
         layout.addSpacing(4)
         layout.addWidget(QLabel('Feedback Template:'))
-        layout.addWidget(QTextEdit())
+        layout.addWidget(self.feedback_temp)
         layout.addSpacing(4)
         layout.addWidget(QLabel('Output Feedback:'))
-        layout.addWidget(QTextEdit())
+        layout.addWidget(self.feedback_output)
         layout.addSpacing(2)
-        # Alt. method for addWidget
-        student_name = QPushButton('Copy Output Feedback')
-        layout.addWidget(student_name)
+        layout.addWidget(self.copy_output)
         self.setLayout(layout)
+        self.feedback_script()
+        self.copy_output.clicked.connect(self.feedback_script)
+
+    def feedback_script(self):
+        global new_student
+        new_student = ''
+        student_name = self.student.text()
+        feedback = self.feedback_temp.toPlainText()
+        if self.yes_button.isChecked():
+            new_student = self.yes_button.text()
+        self.feedback_output.insertPlainText(new_student)
 
 
 
