6a155d688be57b5bf8e16c5098fe6d054ee7e13b
Carlos Smith <mcmco421@gmail.com>
2021-01-13 02:58:11 +0700

    Update style of login_first_msg box

--- main.py
+++ main.py
@@ -309,10 +309,17 @@ class Window(QWidget):
         msgBox = QMessageBox(self)
         msgBox.setIcon(QMessageBox.Information)
         msgBox.setWindowFlag(Qt.ToolTip)
-        msgBox.setText('Please Login First!')
+        msgBox.setText("<p align='center'>Please Login First!<br>")
         msgBox.setStandardButtons(QMessageBox.Ok)
         msgBox.setDefaultButton(QMessageBox.Ok)
-        msgBox.setStyleSheet('background-color: rgb(53, 53, 53); color: rgb(235, 235, 235);')
+        msgBox.setStyleSheet('QMessageBox {background-color: rgb(53, 53, 53); border-top: 25px solid rgb(115, 115, 115);'
+                             'border-left: 1px solid rgb(115, 115, 115); border-right: 1px solid rgb(115, 115, 115);'
+                             'border-bottom: 1px solid rgb(115, 115, 115)}'
+                             'QLabel {color: rgb(235, 235, 235); padding-top: 30px}'
+                             'QPushButton {background-color: rgb(115, 115, 115); color: rgb(235, 235, 235);'
+                             'border-radius: 11px; padding: 5px; min-width: 5em}'
+                             'QPushButton:pressed {background-color: rgb(53, 53, 53)}'
+                             'QPushButton:hover {border: 0.5px solid white}')
         msgBox.exec()
 
     def feedback_automation(self):
