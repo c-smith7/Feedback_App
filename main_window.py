bc4b61bd7da2d322e4834b88974904728bd78dc4
Carlos Smith <mcmco421@gmail.com>
2021-03-06 00:50:47 +0700

    Added a try except block on startup, to catch any errors with the chromdriver connection, such as the need for chromedriver.exe to be updated.

--- main_window.py
+++ main_window.py
@@ -2,11 +2,11 @@ import json
 import os
 import sys
 import time
-
 from PyQt5 import QtGui, QtCore, Qt
 from PyQt5.QtCore import QObject, pyqtSignal, QRunnable, pyqtSlot, QThreadPool
 from PyQt5.QtGui import QPalette, QColor
 from PyQt5.QtWidgets import *
+from selenium.common.exceptions import SessionNotCreatedException
 from main_widget import Window, Splashscreen
 
 
@@ -578,9 +578,50 @@ class SaveSignatureWorkerThreadNew(QRunnable):
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    splash = Splashscreen()
-    window = MainWindow()
-    window.show()
-    splash.stop(window)
-    app.exec()
+    try:
+        app = QApplication(sys.argv)
+        splash = Splashscreen()
+        window = MainWindow()
+        window.show()
+        splash.stop(window)
+        app.exec()
+    except SessionNotCreatedException as e:
+        msgBox = QMessageBox()
+        msgBox.setWindowModality(QtCore.Qt.WindowModal)
+        msgBox.setWindowFlag(QtCore.Qt.ToolTip)
+        msgBox.setIcon(QMessageBox.Information)
+        msgBox.setText('Update required!')
+        msgBox.setDetailedText(str(e))
+        msgBox.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        ok_button = msgBox.button(QMessageBox.Ok)
+        ok_button.setText('Update')
+        msgBox.setDefaultButton(QMessageBox.Ok)
+        msgBox.setStyleSheet(
+            'QMessageBox {background-color: rgb(53, 53, 53); border-top: 1px solid rgb(115, 115, 115);'
+            'border-left: 1px solid rgb(115, 115, 115); border-right: 1px solid rgb(115, 115, 115);'
+            'border-bottom: 1px solid rgb(115, 115, 115); font-family: "Segoe UI";}'
+            'QLabel {color: rgb(235, 235, 235); padding-top: 10px; font-family: "Segoe UI";}'
+            'QPushButton {background-color: rgb(115, 115, 115); color: rgb(235, 235, 235);'
+            'border-radius: 11px; padding: 5px; min-width: 5em; font-family: "Segoe UI"; font-size: 12px;}'
+            'QPushButton:pressed {background-color: rgb(53, 53, 53)}'
+            'QPushButton:hover {border: 0.5px solid white}')
+        msgBox.exec()
+    except Exception as e:
+        msgBox = QMessageBox()
+        msgBox.setWindowModality(QtCore.Qt.WindowModal)
+        msgBox.setWindowFlag(QtCore.Qt.ToolTip)
+        msgBox.setIcon(QMessageBox.Information)
+        msgBox.setText('Unknown error..')
+        msgBox.setDetailedText(str(e))
+        msgBox.setStandardButtons(QMessageBox.Ok)
+        msgBox.setDefaultButton(QMessageBox.Ok)
+        msgBox.setStyleSheet(
+            'QMessageBox {background-color: rgb(53, 53, 53); border-top: 1px solid rgb(115, 115, 115);'
+            'border-left: 1px solid rgb(115, 115, 115); border-right: 1px solid rgb(115, 115, 115);'
+            'border-bottom: 1px solid rgb(115, 115, 115); font-family: "Segoe UI";}'
+            'QLabel {color: rgb(235, 235, 235); padding-top: 10px; font-family: "Segoe UI";}'
+            'QPushButton {background-color: rgb(115, 115, 115); color: rgb(235, 235, 235);'
+            'border-radius: 11px; padding: 5px; min-width: 5em; font-family: "Segoe UI"; font-size: 12px;}'
+            'QPushButton:pressed {background-color: rgb(53, 53, 53)}'
+            'QPushButton:hover {border: 0.5px solid white}')
+        msgBox.exec()
